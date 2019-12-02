ade242ee77bfe8e250cc24a1cf3a822e5b386adf
Rakesh Mithun <slipknut@gmail.com>
2019-12-03 08:10:22 +1000

    03122019_810am

---
+++ main.js
@@ -0,0 +1,8 @@
+// import { NumericInput } from 'numeric-keyboard'
+// new NumericInput({
+//     type: 'number',
+//     placeholder: 'touch to input',
+//     onInput(value) {
+//         ...
+//     }
+// }).mount('.input')
